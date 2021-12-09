d5423dfb939ade2f99eadb31d52e5b21ab3e8834
tpetts <tpetts@madisoncollege.edu>
2021-12-09 11:56:31 -0600

    Getting all planets, but mapping over data
    getting the same results except they are much thinner now

--- src/app/swapi.service.ts
+++ src/app/swapi.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, pipe, EMPTY } from 'rxjs';
+import { Observable, pipe, EMPTY, zip } from 'rxjs';
 import { tap, map, expand } from 'rxjs/operators';
 
 interface SwapiPlanetDataWeCareAbout {
@@ -23,6 +23,12 @@ export class SwapiService {
     const page1 = this.httpSvc.get<SwapiPlanetDataWeCareAbout>('https://swapi.dev/api/planets')
       .pipe(
         expand(x => x.next ? this.httpSvc.get<SwapiPlanetDataWeCareAbout>(x.next) : EMPTY)
+        , map(x => ({
+            next: x.next
+              , results: x.results.map(y => ({
+                  name: y.name
+              }))
+        }))
       )
     ;
 
